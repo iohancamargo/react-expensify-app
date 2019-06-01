4ab32e4055949cd9e0eb8286b5a9c3094c22eb52
Iohan  Camargo <iohancamargo@MacBook-Pro-de-Iohan.local>
2019-06-01 19:25:34 -0300

    Ajuste para deploy com firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    appId: process.env.FIREBASE_APP_ID,
+    apiKey: process.env.FIREBASE_API_KEY,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+};
+
+
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+
+export { firebase, database as default };
--- src/playground/promises.js
+++ src/playground/promises.js
@@ -14,6 +14,29 @@ console.log('antes');
 
 promise.then((data) => {
     console.log('1',data);
+    return 'some data';
+})
+.then((str) => {
+    console.log('isso esta rodando? ',str); // resposta == isso esta rodadno? some data
+})
+.catch((err) => {
+    console.log('Erro obtido:',err);
+});
+
+promise.then((data) => {
+    console.log('1',data);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            
+            resolve({
+                name: 'Iohan',
+                age : 25
+            });
+        }, 1500);
+    });
+})
+.then((str) => {
+    console.log('isso esta rodando? ',str); // resposta == isso esta rodadno? some data
 })
 .catch((err) => {
     console.log('Erro obtido:',err);
