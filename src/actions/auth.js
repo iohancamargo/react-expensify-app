b68434bcb923c10b671a2de42e6a5b78db0d9fd6
Iohan  Camargo <iohancamargo@MacBook-Pro-de-Iohan.local>
2019-06-02 16:54:07 -0300

    ajustando arq auth

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -5,6 +5,3 @@ export const startLogin = () => {
         return firebase.auth().signInWithPopup(googleAuthProvider);
     };
 };
-
-// client id: 436848872047-fve40i141b0m8pkv868ettpnqdummc9q.apps.googleusercontent.com
-// secret key: gQ-zb775RSJpXtYRmEUh_5eN
