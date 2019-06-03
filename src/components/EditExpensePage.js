556b2cae24d361ce71295f068a3b30f2ea8ce4b2
Iohan  Camargo <iohancamargo@MacBook-Pro-de-Iohan.local>
2019-06-03 01:58:38 -0300

    Adicionando estilos para a aplicação

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -15,14 +15,23 @@ export class EditExpensePage extends React.Component {
         this.props.history.push('/');
     };
 
-    render(){
+    render() {
         return (
             <div>
-                <ExpenseForm
-                    expense={this.props.expense}
-                    onSubmit={this.onSubmit}
-                />
-                <button onClick={this.onRemove}>Remove</button>
+
+                <div className="page-header">
+                    <div className="content-container">
+                        <h1 className="page-header__title">Edit Expense</h1>
+                    </div>
+                </div>
+
+                <div className="content-container">
+                    <ExpenseForm
+                        expense={this.props.expense}
+                        onSubmit={this.onSubmit}
+                    />
+                    <button className="button button--secondary" onClick={this.onRemove}>Remove Expense</button>
+                </div>
             </div>
         );
     }
@@ -34,7 +43,7 @@ const mapStateToProps = (state, props) => ({
 });
 
 const mapDispatchToProps = (dispatch, props) => ({
-    startEditExpense: (id,expense) => dispatch(startEditExpense(id, expense)),
+    startEditExpense: (id, expense) => dispatch(startEditExpense(id, expense)),
     startRemoveExpense: (data) => dispatch(startRemoveExpense(data))
 });
 
